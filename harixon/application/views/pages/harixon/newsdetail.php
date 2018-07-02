caff5b70f80ac0716249acca34ada883775679d5
于杰超 <jiechao.yu@ifchange.com>
2018-07-02 21:56:12 +0800

    修改新闻详情页

--- harixon/application/views/pages/harixon/newsdetail.php
+++ harixon/application/views/pages/harixon/newsdetail.php
@@ -1,5 +1,7 @@
 		<div class="main-content news-wrapper">
             <p class="title"><?php echo $newsdetail['title']?></p>
+            <span class="info" >2018年10月02日    作者：阿金费耶夫</span>
+            <hr style="margin-bottom: 30px;" >
 		    <div class="fixck"><?php echo $newsdetail['content']?></div>
 		</div>
 	</div>
@@ -10,8 +12,14 @@
         color: #888;
     }
     .news-wrapper p.title{
-        font-size: 36px;
+        font-size: 24px;
         color: #313131;
+        text-align: center;
+    }
+    .news-wrapper .info{
+        display:  block;
+        text-align:  center;
+        margin-top: 10px;
     }
     .news-wrapper .item{
         margin-top: 15px;
